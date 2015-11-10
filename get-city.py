3a08899259a8882cbbc1f8a914a5b740b361212a
Israel Blancas <iblancasa@gmail.com>
2015-11-10 20:44:14 +0100

    Adding functions to get big or small cities. Warning when API is excedeed

--- get-city.py
+++ get-city.py
@@ -3,27 +3,100 @@
 import os
 import urllib, json
 
-id = os.environ.get('GH_ID')
-secret = os.environ.get('GH_SECRET')
+idGH = os.environ.get('GH_ID')
+secretGH = os.environ.get('GH_SECRET')
+
 
 users = []
-city = "Granada"
+names = []
 
-page = 1
-more = True
-
-while more:
-    url = "https://api.github.com/search/users?client_id="+id+"&client_secret="+secret+ \
-        "&q=sort:followers+type:user+location:"+city+"&per_page=100&page="+str(page)
+def getSizeCity(city):
+    url = "https://api.github.com/search/users?client_id="+idGH+"&client_secret="+secretGH+ \
+        "&q=sort:followers+type:user+location:"+city
 
     response = urllib.urlopen(url)
     data = json.loads(response.read())
-    if len(data['items'])>0:
-        users = users + data['items']
-        page = page + 1
+    return data['total_count']
+
+
+
+
+def addUsers(new_users):
+    global users
+    global names
+    for user in new_users:
+        if not user["login"] in names:
+            users.append(user)
+            names.append(user["login"])
+
+
+
+def read_API(url):
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+
+    if "message" in data:
+        print("API MESSAGE: "+data["message"])
+        if "API rate limit exceeded for" in data["message"]:
+            exit()
+    return data
+
+
+
+#Get All users from city
+def getUsers(city):
+    total_users=getSizeCity(city)
+    page=1
+    more=True
+    global users
+
+
+    if total_users<1000:
+        while more:
+            url = "https://api.github.com/search/users?client_id="+id+"&client_secret="+secret+ \
+                "&q=type:user+location:"+city+"&per_page=100&page="+str(page)
+            data = read_API(url)
+
+            if len(data['items'])>0:
+                addUsers(data['items'])
+                page += 1
+            else:
+                more = False
     else:
-        more = False
+        repos_limit = 999999999
 
 
 
-print len(users)
+        while len(users)<total_users:
+            while more and page<=10:
+                url = "https://api.github.com/search/users?client_id="+idGH+"&client_secret="+secretGH+ \
+                    "&q=sort:repositories+type:user+repos:<"+str(repos_limit)+"+location:"+city+"&per_page=100&page="+str(page)
+                data = read_API(url)
+
+                print(url)
+                if len(data['items'])>0:
+                    users+= data['items']
+                    page += 1
+                else:
+                    more = False
+                print("TOTAL USERS "+str(total_users))
+                print("USUARIOS CONTADOS "+str(len(users)))
+
+            print(len(users)<total_users)
+
+            if len(users)<total_users:
+                response = urllib.urlopen(users[len(users)-1]["repos_url"]+"?client_id="+idGH+"&client_secret="+secretGH)
+                data = json.loads(response.read().decode('utf-8'))
+                repos_limit=len(data)+1
+                more=True
+                page=1
+        return users
+
+
+
+
+users=getUsers("Barcelona")
+print(len(users))
+
+
+
