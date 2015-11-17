a191036f5be262c729e4a3ca9fc37670915308a5
Israel Blancas <iblancasa@gmail.com>
2015-11-17 13:10:45 +0100

    Adding License and fixing a small error

--- GitHubCity.py
+++ GitHubCity.py
@@ -10,7 +10,7 @@ Original idea: https://github.com/JJ/github-city-rankings
 License:
 
 The MIT License (MIT)
-    Copyright (c) 2015 Israel Blancas (http://iblancasa.com/)
+    Copyright (c) 2015 Israel Blancas @iblancasa (http://iblancasa.com/)
 
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software
     and associated documentation files (the “Software”), to deal in the Software without
